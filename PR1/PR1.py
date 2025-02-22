7c408eac59fb9572c638d8c707639d8d2771ba3d
PavloBorshch <156854944+PavloBorshch@users.noreply.github.com>
2025-02-22 16:27:22 +0200

    task #1 completed

---
+++ PR1/PR1.py
@@ -0,0 +1,24 @@
+def custom_zip(*iterables):
+    iterators = [iter(it) for it in iterables]
+    while iterators:
+        result = []
+        for it in iterators:
+            try:
+                result.append(next(it))
+            except StopIteration:
+                return
+        yield tuple(result)
+
+# Приклад використання
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+list3 = [True, False, None]
+
+for item in custom_zip(list1, list2, list3):
+    print(item)
+
+# Додаткові тестові випадки
+list4 = [10, 20, 30, 40]
+list5 = ['x', 'y']
+
+print(list(custom_zip(list4, list5)))  # Очікуваний результат: [(10, 'x'), (20, 'y')]
